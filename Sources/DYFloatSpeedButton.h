1c875a91509f71edcddef263f59ade3105b187c6
xiaoye-debug <xiaoye20020307@gmail.com>
2026-09-20 02:04:25 +0800

    Fix float speed button forward declarations

---
+++ Sources/DYFloatSpeedButton.h
@@ -0,0 +1,15 @@
+#ifndef DY_FLOAT_SPEED_BUTTON_H
+#define DY_FLOAT_SPEED_BUTTON_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+static void DYToolsInstallFloatSpeedButton(UIViewController *controller);
+static void DYToolsRemoveFloatSpeedButton(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
